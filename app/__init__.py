a75eaeea127879d65af614056a390ff3955584a4
manuel borowski <emmanuel.borowski@gmail.com>
2025-12-18 11:18:40 +0100

    0.18: on linux, when disconnecting the scanner, chromium loses the serial port

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,9 @@ from typing import Callable
 # 0.15: mobile, able to update registration.
 # 0.16: update login screen
 # 0.17: rfidserial: show warning when scanner is disconnected
+# 0.18: on linux, when disconnecting the scanner, chromium loses the serial port
 
-version = "0.17"
+version = "0.18"
 
 app = Flask(__name__, instance_relative_config=True, template_folder='presentation/template/')
 
--- app/static/js/project/timeregistration.js
+++ app/static/js/project/timeregistration.js
@@ -45,7 +45,7 @@ const __reload_page = () => {
 const __scanner_init = () => {
     rfid_serial.connect(async data => {
         if (data.type === "state") {
-            if (data.value === false) new AlertPopup("warning", "Opgelet, verbind de badgescanner met de laptop aub!", 2000)
+            if (data.value === false) new AlertPopup("warning", "Opgelet, verbind de badgescanner met de laptop aub!<br>En druk op de toetsen ctrl + r", 2000);
         } else if (data.type === "rfid") {
             await fetch_post("registration.registration", {location_key: "timeregistration", rfid: data.rfid, timestamp: (new Date()).toJSON().substring(0, 19)})
         }
